4c0e8caa7702faad3236a3dcb9ca10b6f14751d1
shamil <shamil880699@gmail.com>
2024-10-07 09:40:26 +0530

    first commit

---
+++ react-todo-backend/src/server.ts
@@ -0,0 +1,91 @@
+import express,{Request,Response}  from "express";
+import cors from "cors";
+import { todo } from "node:test";
+
+
+const app=express()
+
+const PORT=3001;
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+interface Todo{
+    id:number;
+    title:string;
+    completed:boolean;
+}
+
+let todos:Todo[]=[]
+
+app.get('/api/todos',(req:Request,res:Response)=>{
+    res.json(todos)
+})
+
+app.post('/api/todos',(req: Request,res: Response)=>{
+    const {title}=req.body
+    let repeat=todos.find((t)=>t.title===title)
+    if(repeat){
+        return
+    }
+
+    if(!title){
+     res.status(400).json({error:'invalid title'})
+    }
+
+    const newTodo:Todo={
+        id:todos.length+1,
+        title,
+        completed:false
+    }
+    todos.push(newTodo)
+    res.status(201).json(newTodo)
+})
+
+app.put('/api/todos/:id',(req:Request,res:Response)=>{
+    const {id}=req.params
+    const {title,completed}=req.body
+
+    const todoIndex= todos.findIndex(t=>t.id===parseInt(id))
+
+    if(title && typeof title!== "string"){
+        res.status(400).json({error:'Invalid title'})
+    }
+    if(completed !== undefined && typeof completed !== 'boolean'){
+        res.status(400).json({error:"Invalid completed status"})
+    }
+ 
+    todos[todoIndex]={...todos[todoIndex],...req.body}
+    res.json(todos[todoIndex])
+
+})
+
+app.delete('/api/todos/:id',(req:Request,res:Response)=>{
+    const {id}=req.params;
+    const todoIndex=todos.findIndex((t)=>t.id===parseInt(id))
+    if(todoIndex=== -1){
+        res.status(400).json({error:'todo not found'})
+    }
+
+    todos.splice(todoIndex,1)
+    res.status(204).send()
+})
+
+app.put('/api/todos_edit/:id',(req:Request,res:Response)=>{
+    const id=parseInt(req.params.id)
+
+    const todoIndex=todos.findIndex(t=>t.id===id)
+    if(todoIndex!==-1){
+        todos[todoIndex]={...todos[todoIndex],...req.body}
+        res.json(todos[todoIndex])
+    } else {
+        res.status(404).json({ message: 'Todo not found' })
+    }
+})
+   
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
---
+++ react-todo-frontend/src/components/TodoList.tsx
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './TodoListStyle.css'
+
+
+const TodoList: React.FC = () => {
+
+    interface Todo {
+        id: number;
+        title: string;
+        completed: boolean
+    }
+    const [todos, setTodos] = useState<Todo[]>([])
+    const [newTodo, setNewTodo] = useState<string>('')
+    const [editingTodo,setEditingTodo]=useState< Todo |null>(null)
+    const [editingTitle,setEditingTitle]=useState<string>('')
+    useEffect(()=>{
+        fetchTodos();
+    },[]);
+
+    const fetchTodos = async () => {
+        try {
+            const response = await axios.get('http://localhost:3001/api/todos')
+            setTodos(response.data)
+        } catch (error) {
+            console.error('Error fetching todos : ', error)
+        }
+    }
+
+
+    const addTodo = async () => {
+
+        if (!newTodo.trim()) return
+
+        try {
+
+            const response = await axios.post('http://localhost:3001/api/todos', { title: newTodo })
+            console.log([...todos, response.data])
+            setTodos([...todos, response.data])
+            setNewTodo('')
+
+
+        } catch (error) {
+            console.error('Error adding todos : ', error)
+        }
+    }
+
+    const toggleTodo = async (id: number) => {
+
+        try {
+            const todo = todos.find(t => t.id === id)
+
+            if (!todo) return;
+            const response = await axios.put(`http://localhost:3001/api/todos/${id}`, { ...todo, completed: !todo.completed });
+            setTodos(todos.map(t => t.id === id ? response.data : t))
+
+
+        } catch (error) {
+            console.log('Error toggleing todo : ', error)
+        }
+
+    }
+
+    const deleteTodo = async (id: number) => {
+        try {
+            await axios.delete(`http://localhost:3001/api/todos/${id}`);
+            setTodos(todos.filter(t => t.id !== id))
+
+        } catch (error) {
+            console.error('Error deleting todo:', error)
+        }
+    }
+
+    const editTodo=async(todo:Todo)=>{
+
+        try {
+            setEditingTodo(todo)
+            setEditingTitle(todo.title)
+            
+        } catch (error) {
+            console.error('Error editing todo:', error)
+        }
+
+    }
+
+    const updateTodo = async()=>{
+        if (!editingTodo || !editingTitle.trim()) return;
+        
+        try {
+            const response=await axios.put(`http://localhost:3001/api/todos_edit/${editingTodo.id}`,{...editingTodo,title:editingTitle})
+
+            setTodos(todos.map(t=>(t.id===editingTodo.id?response.data:t)))
+
+            setEditingTodo(null);
+            setEditingTitle('');
+
+        } catch (error) {
+            console.error('Error updating todo:', error);
+        }
+
+    }
+
+
+    return (
+        <div className="todo-container">
+            <h1 className="todo-header">Todo List</h1>
+    
+            <div>
+                <input
+                    type="text"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    placeholder="Add new todo"
+                    className="todo-input"
+                />
+                <button onClick={addTodo} className="add-button">Add Todo</button>
+            </div>
+    
+            <ul className="todo-list">
+                {todos.map(todo => (
+                    <li key={todo.id} className="todo-item">
+                        <div>
+                            <input
+                                type="checkbox"
+                                onChange={() => toggleTodo(todo.id)}
+                                className="todo-checkbox"
+                                checked={todo.completed}
+                            />
+
+                            {
+                                editingTodo?.id ===todo.id?(
+                                    <>
+                                    <input
+                                    type="text"
+                                    value={editingTitle}
+                                    onChange={(e)=>setEditingTitle(e.target.value)}
+                                    className="todo-edit-input"
+                                    />
+                                <button onClick={updateTodo} className="delete-button"  >save</button>
+                                    </>
+
+                                ):(
+                                    <span className={`todo-title ${todo.completed ? 'completed' : ''}`}>
+                                {todo.title}
+                                </span>
+                                    
+
+                                )
+                                
+                            }
+
+
+                            
+                        </div>
+                        <button onClick={() => deleteTodo(todo.id)} className="delete-button"> delete </button>
+                        <button onClick={() => editTodo(todo)} className="delete-button"> edit </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+
+}
+
+export default TodoList;
